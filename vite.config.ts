18641c428f2f86ac0ab09990cc497e886cb2c07f
Leonardo Magdarag <leo.baluyot.magdarag039@gmail.com>
2026-09-23 07:07:20 +0800

    Changed vite base

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const repoName = env.GITHUB_REPOSITORY?.split('/')[1] ?? 'Portfolio-better';
-  const base = env.GITHUB_ACTIONS === 'true' ? `/${repoName}/` : '/';
+  const base = '/';
   const projectRoot = path.dirname(fileURLToPath(import.meta.url));
 
   return {
